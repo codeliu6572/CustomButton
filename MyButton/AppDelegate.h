03802eb8693ef6547e60315504953d71b2a77298
codeliu6572 <codeliu6572@163.com>
2017-02-07 09:10:18 +0800

    Initial Commit

---
+++ MyButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyButton
+//
+//  Created by 刘浩浩 on 2017/2/7.
+//  Copyright © 2017年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyButton
+//
+//  Created by 刘浩浩 on 2017/2/7.
+//  Copyright © 2017年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
